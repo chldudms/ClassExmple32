10b8900ed1a3c18f6dcc5e46d39aabe8a9daa329
chldudms <yueun0337@gmail.com>
2025-06-12 14:07:42 +0900

    상속 시 생성자 예

--- Program.cs
+++ Program.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ClassExample32
 {
     class Program
     {
+        class Parent
+        {
+            public Parent() {  Console.WriteLine("Parent()");}
+            public Parent(int param) { Console.WriteLine("Parent(int param)"); }
+            public Parent(string param) { Console.WriteLine("Parent(string param)"); }
+        }
+
+        class Child : Parent
+        {
+            public Child() : base(10)
+            {
+                Console.WriteLine("Child():base(10)");
+            }
+            public Child(string input) : base(input)
+            {
+                Console.WriteLine("Child(string input) : base(input)");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Child childA = new Child();
+            Child childB = new Child("string");
         }
     }
 }
